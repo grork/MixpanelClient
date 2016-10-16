14a33ae030cb17f6d3e32fac7eeaf28c6d419a9e
Dominic Hopton <d.hopton@codevoid.net>
2016-10-16 15:07:00 -0700

    Added simple EncodeJson method intended to be used internally, that Base64 encodes a JSON object.
    Added simple unit test to validate the encoding.
    
    Note, the samples on mixpanel.com encode the new lines, and JsonObject::Stringify does not have any controls over that, so it removes newlines, and " ".

---
+++ MixpanelCppCX/PayloadEncoder.cpp
@@ -0,0 +1,19 @@
+#include "pch.h"
+#include "payloadencoder.h"
+
+using namespace Platform;
+using namespace Windows::Security::Cryptography;
+using namespace Windows::Data::Json;
+
+using namespace CodevoidN::Utilities::Mixpanel;
+
+String^ CodevoidN::Utilities::Mixpanel::EncodeJson(JsonObject^ payload)
+{
+    auto payloadAsString = payload->Stringify();
+
+    auto payloadAsBuffer = CryptographicBuffer::ConvertStringToBinary(payloadAsString, BinaryStringEncoding::Utf8);
+    
+    auto encodedPayload = CryptographicBuffer::EncodeToBase64String(payloadAsBuffer);
+
+    return encodedPayload;
+}
--- UnitTests/UnitTest.cpp
+++ UnitTests/UnitTest.cpp
@@ -1,9 +1,11 @@
 #include "pch.h"
 #include "CppUnitTest.h"
 #include "MixpanelClient.h"
-#include "MixpanelApiKeys.h"
+#include "PayloadEncoder.h"
 
+using namespace CodevoidN::Utilities::Mixpanel;
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+using namespace Windows::Data::Json;
 
 namespace CodevoidN { namespace Tests { namespace Mixpanel
 {
@@ -12,8 +14,15 @@ namespace CodevoidN { namespace Tests { namespace Mixpanel
     public:
         TEST_METHOD(CanInstantiate)
         {
-            auto client = ref new CodevoidN::Utilities::Mixpanel::MixpanelClient();
+            auto client = ref new MixpanelClient();
             Assert::IsNotNull(client, "Couldn't instantiate client");
+        }
+
+        TEST_METHOD(CanEncodePayloadCorrectly)
+        {
+            JsonObject^ payload = JsonObject::Parse(L"{ \"event\": \"Signed Up\", \"properties\": { \"distinct_id\": \"13793\", \"token\": \"e3bc4100330c35722740fb8c6f5abddc\", \"Referred By\": \"Friend\" } }");
+            auto encodedPayload = EncodeJson(payload);
+            Assert::AreEqual(L"eyJldmVudCI6IlNpZ25lZCBVcCIsInByb3BlcnRpZXMiOnsiZGlzdGluY3RfaWQiOiIxMzc5MyIsInRva2VuIjoiZTNiYzQxMDAzMzBjMzU3MjI3NDBmYjhjNmY1YWJkZGMiLCJSZWZlcnJlZCBCeSI6IkZyaWVuZCJ9fQ==", encodedPayload, "Not equal");
         }
     };
 } } }
