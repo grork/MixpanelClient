353beab03d71db3a1f7f2da43395fdefc3ebe79c
Dominic Hopton <1811056+grork@users.noreply.github.com>
2018-09-02 16:00:27 -0700

    Adding Constants for user identity fields

--- MixpanelCppCX/EngageConstants.cpp
+++ MixpanelCppCX/EngageConstants.cpp
@@ -9,6 +9,13 @@ constexpr auto TIME_OPTION_NAME = L"$time";
 constexpr auto IGNORE_TIME_OPTION_NAME = L"$ignore_time";
 constexpr auto IGNORE_ALIAS_OPTION_NAME = L"$ignore_alias";
 
+constexpr auto FIRST_NAME_PROPERTY_NAME = L"$first_name";
+constexpr auto LAST_NAME_PROPERTY_NAME = L"$last_name";
+constexpr auto NAME_PROPERTY_NAME = L"$name";
+constexpr auto CREATED_PROPERTY_NAME = L"$created";
+constexpr auto EMAIL_PROPERTY_NAME = L"$email";
+constexpr auto PHONE_PROPERTY_NAME = L"$phone";
+
 String^ EngageOptionNames::Ip::get()
 {
     return StringReference(IP_OPTION_NAME);
@@ -27,4 +34,34 @@ String^ EngageOptionNames::IgnoreTime::get()
 String^ EngageOptionNames::IgnoreAlias::get()
 {
     return StringReference(IGNORE_ALIAS_OPTION_NAME);
+}
+
+String^ EngageReservedPropertyNames::FirstName::get()
+{
+    return StringReference(FIRST_NAME_PROPERTY_NAME);
+}
+
+String^ EngageReservedPropertyNames::LastName::get()
+{
+    return StringReference(LAST_NAME_PROPERTY_NAME);
+}
+
+String^ EngageReservedPropertyNames::Name::get()
+{
+    return StringReference(NAME_PROPERTY_NAME);
+}
+
+String^ EngageReservedPropertyNames::Created::get()
+{
+    return StringReference(CREATED_PROPERTY_NAME);
+}
+
+String^ EngageReservedPropertyNames::Email::get()
+{
+    return StringReference(EMAIL_PROPERTY_NAME);
+}
+
+String^ EngageReservedPropertyNames::Phone::get()
+{
+    return StringReference(PHONE_PROPERTY_NAME);
 }
--- MixpanelCppCX/EngageConstants.h
+++ MixpanelCppCX/EngageConstants.h
@@ -12,4 +12,18 @@ namespace Codevoid::Utilities::Mixpanel {
         static property Platform::String^ IgnoreTime { Platform::String^ get(); }
         static property Platform::String^ IgnoreAlias { Platform::String^ get(); }
     };
+
+    public ref class EngageReservedPropertyNames sealed
+    {
+    private:
+        EngageReservedPropertyNames() {};
+
+    public:
+        static property Platform::String^ FirstName { Platform::String^ get();  }
+        static property Platform::String^ LastName { Platform::String^ get();  }
+        static property Platform::String^ Name { Platform::String^ get();  }
+        static property Platform::String^ Created { Platform::String^ get();  }
+        static property Platform::String^ Email { Platform::String^ get();  }
+        static property Platform::String^ Phone { Platform::String^ get();  }
+    };
 }
