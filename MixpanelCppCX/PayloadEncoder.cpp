d93d07595b8dcd017c9a31556f80fcd0082c6fea
Dominic Hopton <d.hopton@codevoid.net>
2019-05-25 16:08:20 -0700

    Provide fallback handling when stringification of time fails on certain machines
    Based on a report from a wonderful customer, it appears that there are certain situations where this call -- when supplied with the Etc/UTC timezone will cause the platform to throw a 'Catastrophic Error' COMException. It's unclear why -- the customer reported it
    on Windows 10, 1803 when in the Australian region/timezone.
    
    While unable to repro locally on any of my devices, it was confirmed that using the 'no timezone adjustment' version of the API results in a successful stringification. So, lets try the 'right way' (Since Mixpanel says to supply this information in UTC), and if it fails,
    accept that the time could be off by ~12 hours in either direction.
    
    Additional: Tweak the declaration to no longer pass by reference, if only for sanity sake

--- MixpanelCppCX/PayloadEncoder.cpp
+++ MixpanelCppCX/PayloadEncoder.cpp
@@ -18,7 +18,7 @@ String^ Codevoid::Utilities::Mixpanel::EncodeJson(IJsonValue^ payload)
     return encodedPayload;
 }
 
-String^ Codevoid::Utilities::Mixpanel::DateTimeToMixpanelDateFormat(const DateTime& time)
+String^ Codevoid::Utilities::Mixpanel::DateTimeToMixpanelDateFormat(const DateTime time)
 {
     // Format from mixpanel:
     // YYYY-MM-DDThh:mm:ss
@@ -33,5 +33,24 @@ String^ Codevoid::Utilities::Mixpanel::DateTimeToMixpanelDateFormat(const DateTi
     //-{month.full}-{day.integer(2)}T:{longtime}
     static DateTimeFormatter^ formater = ref new DateTimeFormatter(L"{year.full}-{month.integer(2)}-{day.integer(2)}T{hour.integer(2)}:{minute.integer(2)}:{second.integer(2)}");
 
-    return formater->Format(time, L"Etc/UTC");
+    String^ result = nullptr;
+    try
+    {
+        // Based on a report from a wonderful customer, it appears that there
+        // are certain situations where this call -- when supplied with the
+        // Etc/UTC timezone will cause the platform to throw a 'Catastrophic
+        // Error' COMException. It's unclear why -- the customer reported it
+        // on Windows 10, 1803 when in the Australian region/timezone. While
+        // unable to repro locally on any of my devices, it was confirmed that
+        // using the 'no timezone adjustment' version of the API results in a
+        // successful stringification. So, lets try the 'right way' (Since
+        // Mixpanel says to supply this information in UTC), and if it fails,
+        // accept that the time could be off by ~12 hours in either direction
+        result = formater->Format(time, L"Etc/UTC");
+    }
+    catch (COMException^ e) {
+        result = formater->Format(time);
+    }
+
+    return result;
 }
--- MixpanelCppCX/PayloadEncoder.h
+++ MixpanelCppCX/PayloadEncoder.h
@@ -2,5 +2,5 @@
 
 namespace Codevoid::Utilities::Mixpanel {
     Platform::String^ EncodeJson(Windows::Data::Json::IJsonValue^ payload);
-    Platform::String^ DateTimeToMixpanelDateFormat(const Windows::Foundation::DateTime& time);
+    Platform::String^ DateTimeToMixpanelDateFormat(const Windows::Foundation::DateTime time);
 }
